b87f282052ffeb4619e1b6bde70b98520137652d
Daniil <cvb.amaho@gmail.com>
2021-03-30 10:31:49 +0200

    Change

--- main.js
+++ main.js
@@ -1,148 +1,132 @@
-const Apify = require('apify');
+const Apify = require('apify')
 
 Apify.main(async () => {
+    const keyword = 'phone'
 
-  const keyword = 'phone'
+    const StartUrl = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='
 
-  const StartUrl = 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='
+    const StartProductUrl = 'https://www.amazon.com/dp/'
 
-  const StartProductUrl = 'https://www.amazon.com/dp/'
+    const StartOfferUrl = 'https://www.amazon.com/gp/offer-listing/'
 
-  const StartOfferUrl = 'https://www.amazon.com/gp/offer-listing/'
+    const requestQueue = await Apify.openRequestQueue();
 
-  const requestQueue = await Apify.openRequestQueue()
+    await requestQueue.addRequest({
+        url: StartUrl + keyword,
+        userData: {
+            label: 'start',
+            keyword,
+        },
 
-  await requestQueue.addRequest({
-    url: StartUrl + keyword,
-    userData: {
-      label: 'start',
-      keyword
-    }
+    });
 
-  })
+    const crawler = new Apify.PuppeteerCrawler({
 
-  const crawler = new Apify.PuppeteerCrawler({
+        requestQueue,
+        handlePageFunction: async ({ page, request }) => {
+            if (request.userData.label === 'start') {
+                console.log('Start page is:', request.url)
 
-    requestQueue,
-    handlePageFunction: async ({ page, request }) => {
+                try {
+                    await page.waitForSelector('.s-result-list', { timeout: 10000 })
 
-        if (request.userData.label === 'start') {
-          console.log('Start page is:', request.url)
+                    const asins = await page.$$eval('.s-result-item', async (items) => {
+                        return items.map((item) => item.dataset.asin).filter((item) => item !== '' && item !== undefined)
+                    });
 
-          try {
-            await page.waitForSelector('.s-result-list', { timeout: 10000 })
+                    const items = asins.map((asin) => {
+                        const productUrl = StartProductUrl + asin
+                        const sellerUrl = StartOfferUrl + asin
+                        return { asin, productUrl, sellerUrl, keyword: request.userData.keyword }
+                    });
 
-            const asins = await page.$$eval('.s-result-item', async (items) => {
-                return items.map((item) => item.dataset.asin).filter((item) => item !== '' && item !== undefined)
-            })
+                    for (const item of items) {
+                        await requestQueue.addRequest({
+                            url: item.productUrl,
+                            userData: {
+                                label: 'product',
+                                asin: item.asin,
+                                keyword: item.keyword,
+                                productUrl: item.productUrl,
+                                sellerUrl: item.sellerUrl,
+                            },
+                        });
+                    }
+                } catch (err) {
+                    console.log(err);
+                    await dataset.pushData({
+                        url: request.url,
+                        status: 'No results',
+                    });
+                }
 
+                console.log('Keep crawling.')
+            } else if (request.userData.label === 'product') {
+                console.log(`Go to product page: ${request.url}`)
 
-          const items = asins.map((asin) => {
-            const productUrl = StartProductUrl + asin
-            const sellerUrl = StartOfferUrl + asin
-            return {asin, productUrl, sellerUrl, keyword: request.userData.keyword}
-          })
+                const PageInfo = await page.evaluate(() => {
+                    const titleEl = document.getElementById('productTitle')
 
-          for (const item of items) {
-            await requestQueue.addRequest({
-              url: item.productUrl,
-              userData: {
-                label: 'product',
-                asin: item.asin,
-                keyword: item.keyword,
-                productUrl: item.productUrl,
-                sellerUrl: item.sellerUrl
-              }
-            })
-          }
-        } catch (err) {
-          console.log(err)
-          await dataset.pushData({
-              url: request.url,
-              status: 'No results',
-          });
-        }
+                    if (!titleEl) {
+                        return false
+                    }
 
-        console.log('Keep crawling.')
-      }
+                    const title = titleEl.innerText
+                    const url = document.URL
+                    const description = document.getElementById('productDescription') ? document.getElementById('productDescription').innerText : 'No description.'
 
+                    return { title, url, description }
+                });
 
-      else if (request.userData.label === 'product') {
-        console.log('Go to product page: ' + request.url)
+                const { asin, keyword, productUrl } = request.userData
 
+                await requestQueue.addRequest({
+                    url: request.userData.sellerUrl,
+                    userData: {
+                        label: 'seller',
+                        asin,
+                        keyword,
+                        productUrl,
+                        title: PageInfo.title,
+                        description: PageInfo.description,
+                    },
+                });
 
-        const PageInfo  = await page.evaluate(() => {
+                console.log(`End with ${request.url}`)
+            } else if (request.userData.label === 'seller') {
+                console.log(`Go to  seller page: ${request.url}`)
 
-            const titleEl = document.getElementById('productTitle')
+                const offers = await page.evaluate(() => {
+                    const price = document.querySelector('span.a-price > span') ? document.querySelector('span.a-price > span').innerText : 'no Price'
+                    const seller = document.querySelector('#sellerProfileTriggerId') ? document.querySelector('#sellerProfileTriggerId').innerText : 'Amazon'
 
-            if (!titleEl) {
-                return false
+                    return { price, seller }
+                });
+
+                const { asin, keyword, productUrl, title, description } = request.userData;
+
+                const item = {
+                    title,
+                    itemUrl: productUrl,
+                    description,
+                    keyword,
+                    asin,
+                    price: offers.price,
+                    seller: offers.seller,
+                };
+
+                await Apify.pushData(item);
             }
+        },
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed 4 times`)
 
-            const title = titleEl.innerText
-            const url = document.URL
-            const description = document.getElementById('productDescription') ? document.getElementById('productDescription').innerText : 'No description.'
+            await Apify.pushData({
+                url: request.url,
+                errors: request.errorMessages,
+            })
+        },
+    })
 
-
-
-            return { title, url, description }
-        })
-
-        const { asin, keyword, productUrl } = request.userData
-
-        await requestQueue.addRequest({
-          url: request.userData.sellerUrl,
-          userData: {
-            label: 'seller',
-            asin,
-            keyword,
-            productUrl,
-            title: PageInfo.title,
-            description: PageInfo.description
-          }
-        })
-
-        console.log('End with ' + request.url)
-
-
-      }
-
-      else if (request.userData.label === 'seller') {
-        console.log('Go to  seller page: ' + request.url)
-
-        const offers = await page.evaluate(() => {
-
-            const price = document.querySelector('span.a-price > span')  ? document.querySelector('span.a-price > span').innerText : 'no Price'
-            const seller = document.querySelector('#sellerProfileTriggerId') ? document.querySelector('#sellerProfileTriggerId').innerText : 'Amazon'
-
-            return { price, seller }
-
-      })
-
-          const { asin, keyword, productUrl, title, description} = request.userData
-
-          const item = {
-            title,
-            itemUrl: productUrl,
-            description,
-            keyword,
-            asin,
-            price: offers.price,
-            seller: offers.seller
-          }
-
-          await Apify.pushData(item)
-        }
-    },
-    handleFailedRequestFunction: async ({ request }) => {
-        console.log(`Request ${request.url} failed 4 times`)
-
-        await Apify.pushData({
-            url: request.url,
-            errors: request.errorMessages,
-        })
-    }
-  })
-
-  await crawler.run()
-})
+    await crawler.run()
+});
